6ed66aa492e9e32f93ec8bce398d013bcce0457d
Haidar <hilmyahmadhaidar@gmail.com>
2024-04-26 07:29:58 +0700

    feat | adding informasi and gate for administrator

---
+++ app/Http/Controllers/API/InformasiController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Informasi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class InformasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (!Auth::check()) {
+            return response([
+                'status' => 'error',
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all data informasi success!',
+            'data' => Informasi::latest()->get()
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'judul' => 'required|string',
+            'deskripsi' => 'required|string',
+            'tanggal' => 'required|string',
+            'waktu' => 'required|string',
+        ]);
+
+        $data['user_id'] = Auth::id();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Create data informasi success!',
+            'data' => Informasi::create($data)
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Informasi $informasi)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get data informasi success!',
+            'data' => $informasi
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Informasi $informasi)
+    {
+        $data = $request->validate([
+            'judul' => 'required|string',
+            'deskripsi' => 'required|string',
+            'tanggal' => 'required|string',
+            'waktu' => 'required|string',
+        ]);
+
+        $informasi->update($data);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Update data informasi success!',
+            'data' => $informasi
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Informasi $informasi)
+    {
+        $informasi->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Delete data informasi success!'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\InformasiController;
 use App\Http\Controllers\API\WargaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,11 +9,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'show']);
+
+    // Warga
     Route::get('/warga', [WargaController::class, 'index']);
     Route::post('/warga', [WargaController::class, 'store']);
     Route::get('/warga/{warga}', [WargaController::class, 'show']);
     Route::put('/warga/{warga}', [WargaController::class, 'update']);
     Route::delete('/warga/{warga}', [WargaController::class, 'destroy']);
+
+    // Informasi
+    Route::get('/informasi', [InformasiController::class, 'index']);
+    Route::post('/informasi', [InformasiController::class, 'store']);
+    Route::get('/informasi/{informasi}', [InformasiController::class, 'show']);
+    Route::put('/informasi/{informasi}', [InformasiController::class, 'update']);
+    Route::delete('/informasi/{informasi}', [InformasiController::class, 'destroy']);
+});
+
+Route::group(['middleware' => 'is_admin', 'auth:sanctum'], function () {
+    Route::get('/admin', function () {
+        return response()->json([
+            'message' => 'You are an admin!',
+        ]);
+    });
 });
 
 Route::post('/register', [AuthController::class, 'register']);
